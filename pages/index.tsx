467774b71f13a890da116983db4c1234bccca4a7
MichaelNgCen <michaelngcen@yahoo.com>
2023-05-24 18:19:59 -0400

    protect home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { signOut, getSession } from "next-auth/react";
 
-const inter = Inter({ subsets: ['latin'] })
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className="text-4xl font-bold text-center">Hello World</h1>
+      <p className="text-center text-xl mt-4 text-white">
+        Welcome back, {user?.name ?? "User"}!
+      </p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Sign Out
+      </button>
     </>
-  )
+  );
 }
