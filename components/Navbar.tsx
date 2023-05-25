019984306cb7171c12427c121e34738b5fca4968
MichaelNgCen <michaelngcen@yahoo.com>
2023-05-24 21:53:49 -0400

    scroll for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,29 @@ import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
 
 import MobileMenu from "./MobileMenu";
 import NavBarItem from "./NavBarItem";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 66;
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const toggleMobileMenu = useCallback(() => {
     setShowMobileMenu((currentMobileMenu) => !currentMobileMenu);
@@ -19,7 +36,11 @@ const Navbar = () => {
 
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90 ">
+      <div
+        className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
+          showBackground ? "bg-zinc-900 bg-opacity-90" : ""
+        }`}
+      >
         <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
           <NavBarItem label="Home" />
