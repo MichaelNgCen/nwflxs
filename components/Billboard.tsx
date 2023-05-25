31dd6f8307f2bfe2f34c24d6f02063524179d4f1
MichaelNgCen <michaelngcen@yahoo.com>
2023-05-25 03:27:47 -0400

    add button on billboard

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,6 +1,8 @@
 import useBillboard from "@/hooks/useBillboard";
 import React from "react";
 
+import { AiOutlineInfoCircle } from "react-icons/ai";
+
 const Billboard = () => {
   const { data } = useBillboard();
 
@@ -21,6 +23,12 @@ const Billboard = () => {
         <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
           {data?.description}
         </p>
+        <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition">
+            <AiOutlineInfoCircle className="mr-1" />
+            More Info
+          </button>
+        </div>
       </div>
     </div>
   );
