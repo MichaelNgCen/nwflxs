2cc62f7c3b1ecb23cde501906847946b2df44ca9
MichaelNgCen <michaelngcen@yahoo.com>
2023-05-28 01:49:04 -0400

    style movie card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -10,8 +10,18 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
       <img
         src={data.thumbnailUrl}
         alt={data.title}
-        className="w-full h-full object-cover"
+        className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]"
       />
+      <div
+        className="opacity-0 absolute top-0 transition duration-200 z-10
+invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100"
+      >
+        <img
+          className="cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]"
+          src={data.thumbnailUrl}
+          alt="Thumbnail"
+        />
+      </div>
     </div>
   );
 };
