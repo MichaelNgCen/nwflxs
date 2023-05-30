e48af6308cf15615a6217b7e86134f6e90c91a44
MichaelNgCen <michaelngcen@yahoo.com>
2023-05-30 01:11:04 -0400

    more styling

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsFillPlayBtnFill, BsFillPlayFill } from "react-icons/bs";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -21,6 +22,25 @@ invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:
           src={data.thumbnailUrl}
           alt="Thumbnail"
         />
+        <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
+          <div className="flex flex-row items-center gap-3">
+            <div
+              className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
+              onClick={() => {}}
+            >
+              <BsFillPlayFill size={30} />
+            </div>
+          </div>
+          <p className="text-green-400 font-semibold mt-4">
+            New <span className="text-white">2023</span>
+          </p>
+          <div className="flex flex-row mt-4 gap-2 items-center">
+            <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
+          </div>
+          <div className="flex flex-row mt-4 gap-2 items-center">
+            <p className="text-white text-[10px] lg:text-sm">{data.genres}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
