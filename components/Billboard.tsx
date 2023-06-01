8c7c707a9d11021bf6a30bee93bdf270873b62b5
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-01 14:40:18 -0400

    Fix small bug

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -33,7 +33,9 @@ const Billboard = () => {
           <PlayButton movieId={data?.id} />
           <button
             className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition"
-            onClick={handleToggleMute}
+            onClick={() => {
+              window.location.href = `https://www.google.com/search?q= ${data?.title}`;
+            }}
           >
             <AiOutlineInfoCircle className="mr-1" />
             More Info
--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -14,6 +14,7 @@ const Watch = () => {
       <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
         <AiOutlineArrowLeft
           onClick={() => router.push("/")}
+          size={30}
           className="w-4 md:w-10 text-white cursor-pointer hover:opacity-80 transition"
         />
         <p className="text-white text-1xl md:text-3xl font-bold">
