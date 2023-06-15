6502d902907ebe86df9278c1739ac61b95595336
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-15 19:05:28 -0400

    fix for mobile

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -11,11 +11,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
         <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex">
             <div className="flex flex-col gap-4">
                 <p className="text-white px-3 text-center hover:underline ">Home</p>
-                <p className="text-white px-3 text-center hover:underline ">Series</p>
-                <p className="text-white px-3 text-center hover:underline ">Films</p>
-                <p className="text-white px-3 text-center hover:underline ">New & Popular</p>
-                <p className="text-white px-3 text-center hover:underline ">My List</p>
-                <p className="text-white px-3 text-center hover:underline ">Browse by Language</p>
+                <p className="text-white px-3 text-center hover:underline ">All Music Video</p>
             </div>
         </div>
     );
